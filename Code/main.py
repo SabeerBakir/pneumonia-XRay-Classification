6dd0616add6872fde37594928da5a155717bd365
Kacper Twardowski <kanexer@gmail.com>
2020-04-13 17:31:16 +0100

    Revert "Formatted main"
    This reverts commit 352f16e090350846372b5dd7e50988ddd22c57f7.

--- Code/main.py
+++ Code/main.py
@@ -26,39 +26,38 @@ def main():
     start_time = time.strftime("%d%m%y_%H%M%S")
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "model_name",
-        type=str,
+        "model_name", 
+        type=str, 
         help="Pass name of model as defined in hparams.yaml."
-    )
+        )
     parser.add_argument(
         "--write_data",
-        required=False,
+        required = False,
         default=False,
-        help="Set to true to write_data."
-    )
+                help="Set to true to write_data."
+        )
     args = parser.parse_args()
-    # Parse our YAML file which has our model parameters.
+    # Parse our YAML file which has our model parameters. 
     params = Params("hparams.yaml", args.model_name)
-    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = params.gpu_vis_dev
-    # Check if a GPU is available and use it if so.
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=params.gpu_vis_dev
+    # Check if a GPU is available and use it if so. 
     use_gpu = torch.cuda.is_available()
     device = torch.device("cuda" if use_gpu else "cpu")
-
-    # Load model that has been chosen via the command line arguments.
-    model_module = __import__(
-        '.'.join(['models', params.model_name]),  fromlist=['object'])
+    
+    # Load model that has been chosen via the command line arguments. 
+    model_module = __import__('.'.join(['models', params.model_name]),  fromlist=['object'])
     model = model_module.net()
-    # Send the model to the chosen device.
+    # Send the model to the chosen device. 
     # To use multiple GPUs
     # model = nn.DataParallel(model)
     model.to(device)
     # Grap your training and validation functions for your network.
     train = model_module.train
     val = model_module.val
-
+    
     optimizer = optim.Adam(model.parameters(), lr=params.lr)
-    # Write data if specified in command line arguments.
+    # Write data if specified in command line arguments. 
     if args.write_data:
         data = pd.read_csv('fashionmnist/fashion-mnist_train.csv')
         test_data = pd.read_csv('fashionmnist/fashion-mnist_test.csv')
@@ -66,22 +65,19 @@ def main():
         data = shuffle(data)
         train_data = data.iloc[val_split:]
         val_data = data.iloc[:val_split]
-        train_data.to_csv(os.path.join(
-            params.data_dir, "train.csv"), index=False)
+        train_data.to_csv(os.path.join(params.data_dir, "train.csv"), index=False)
         val_data.to_csv(os.path.join(params.data_dir, "val.csv"), index=False)
-        test_data.to_csv(os.path.join(
-            params.data_dir, "test.csv"), index=False)
+        test_data.to_csv(os.path.join(params.data_dir, "test.csv"), index=False)
 
-    # This is useful if you have multiple custom datasets defined.
+    # This is useful if you have multiple custom datasets defined. 
     Dataset = getattr(Datasets, params.dataset_class)
 
-    transf_train = getattr(model_transforms, params.transforms_train)()
-    transf_val = getattr(model_transforms, params.transforms_val)()
-    train_data = Dataset(params.data_dir + "/train", transform=transf_train)
-    val_data = Dataset(params.data_dir + "/val", transform=transf_val)
+    transf = getattr(model_transforms, params.transforms)()
+    train_data = Dataset(params.data_dir + "/train", transform=transf)
+    val_data = Dataset(params.data_dir + "/val", transform=transf)
 
     train_loader = DataLoader(
-        train_data,
+        train_data, 
         batch_size=params.batch_size,
         shuffle=True
     )
@@ -90,12 +86,9 @@ def main():
         batch_size=params.batch_size,
         shuffle=False
     )
-    if not os.path.exists(params.log_dir):
-        os.makedirs(params.log_dir)
-    if not os.path.exists(params.checkpoint_dir):
-        os.makedirs(params.checkpoint_dir)
-    if not os.path.exists("figs"):
-        os.makedirs("figs")
+    if not os.path.exists(params.log_dir): os.makedirs(params.log_dir)
+    if not os.path.exists(params.checkpoint_dir): os.makedirs(params.checkpoint_dir)
+    if not os.path.exists("figs"): os.makedirs("figs")
 
     val_roc_aucs = []
     val_losses = []
@@ -103,31 +96,27 @@ def main():
     train_roc_aucs = []
     for epoch in range(1, params.num_epochs + 1):
         print("Epoch: {}".format(epoch))
-        # Call training function.
+        # Call training function. 
         train(model, device, train_loader, optimizer)
-        # Evaluate on both the training and validation set.
+        # Evaluate on both the training and validation set. 
         train_loss, train_roc_auc = val(model, device, train_loader)
         val_loss, val_roc_auc = val(model, device, val_loader)
-        # Collect some data for logging purposes.
+        # Collect some data for logging purposes. 
         train_losses.append(float(train_loss))
         train_roc_aucs.append(train_roc_auc)
         val_losses.append(float(val_loss))
         val_roc_aucs.append(val_roc_auc)
 
-        print('\n\ttrain Loss: {:.6f}\ttrain roc_auc: {:.6f} \n\tval Loss: {:.6f}\tval roc_auc: {:.6f}'.format(
-            train_loss, train_roc_auc, val_loss, val_roc_auc))
-        # Here is a simply plot for monitoring training.
-        # Clear plot each epoch
-        fig = plot_training(train_losses, train_roc_aucs,
-                            val_losses, val_roc_aucs)
-        fig.savefig(os.path.join(
-            "figs", "{}_training_vis".format(args.model_name)))
+        print('\n\ttrain Loss: {:.6f}\ttrain roc_auc: {:.6f} \n\tval Loss: {:.6f}\tval roc_auc: {:.6f}'.format(train_loss, train_roc_auc, val_loss, val_roc_auc))
+        # Here is a simply plot for monitoring training. 
+        # Clear plot each epoch 
+        fig = plot_training(train_losses, train_roc_aucs, val_losses, val_roc_aucs)
+        fig.savefig(os.path.join("figs", "{}_training_vis".format(args.model_name)))
         # Save model every few epochs (or even more often if you have the disk space).
         if epoch % 5 == 0:
-            torch.save(model.state_dict(), os.path.join(
-                params.checkpoint_dir, "checkpoint_{}_epoch_{}".format(args.model_name, epoch)))
-    # Some log information to help you keep track of your model information.
-    logs = {
+            torch.save(model.state_dict(), os.path.join(params.checkpoint_dir,"checkpoint_{}_epoch_{}".format(args.model_name,epoch)))
+    # Some log information to help you keep track of your model information. 
+    logs ={
         "model": args.model_name,
         "train_losses": train_losses,
         "train_roc_aucs": train_roc_aucs,
@@ -136,10 +125,10 @@ def main():
         "best_val_epoch": int(np.argmax(val_roc_aucs)+1),
         "model": args.model_name,
         "lr": params.lr,
-        "batch_size": params.batch_size
+        "batch_size":params.batch_size
     }
 
-    with open(os.path.join(params.log_dir, "{}_{}.json".format(args.model_name,  start_time)), 'w') as f:
+    with open(os.path.join(params.log_dir,"{}_{}.json".format(args.model_name,  start_time)), 'w') as f:
         json.dump(logs, f)
 
 
