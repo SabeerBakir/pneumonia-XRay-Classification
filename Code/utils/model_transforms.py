61e134d4446e4658702c2b05b1ed478e3bac3c4c
Kacper Twardowski <kanexer@gmail.com>
2020-04-14 13:52:33 +0100

    Update to args

--- Code/utils/model_transforms.py
+++ Code/utils/model_transforms.py
@@ -27,19 +27,13 @@ def AlexNet_Eval_Transforms():
     ])
 
 def ResNext_Train_Transforms():
-    return transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomResizedCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+    return ResNext_Eval_Transforms()
 
 
 def ResNext_Eval_Transforms():
     return transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(1024),
+        transforms.CenterCrop(1000),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
