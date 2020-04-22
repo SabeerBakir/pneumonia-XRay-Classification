c10f693c80894407abc1632f3eada81d34c442ef
Kacper Twardowski <kanexer@gmail.com>
2020-04-22 15:29:08 +0100

    Updated Model Augments

--- Code/utils/model_augments.py
+++ Code/utils/model_augments.py
@@ -4,20 +4,11 @@ from albumentations.pytorch import ToTensorV2 as AToTensor
 
 
 def CNN_Train_Augments():
-    return transforms.Compose([
-        transforms.Grayscale(),
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor()
-    ])
+    return Default_Train_Augments()
 
 
 def CNN_Eval_Augments():
-    return transforms.Compose([
-        transforms.Grayscale(),
-        transforms.CenterCrop(224),
-        transforms.ToTensor()
-    ])
+    return Default_Eval_Augments()
 
 
 def AlexNet_Train_Augments():
@@ -37,23 +28,11 @@ def ResNext_Eval_Augments():
 
 
 def MLP_Train_Augments():
-    return A.Compose([
-        A.Resize(256, 256),
-        A.CenterCrop(224, 224),
-        A.HorizontalFlip(p=0.2),  # Probability 20%
-        A.ShiftScaleRotate(p=0.2),
-        A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        AToTensor()
-    ])
+    return Default_Train_Augments()
 
 
 def MLP_Eval_Augments():
-    return A.Compose([
-        A.Resize(256, 256),
-        A.CenterCrop(224, 224),
-        A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        AToTensor()
-    ])
+    return Default_Eval_Augments()
 
 
 def Default_Train_Augments():
